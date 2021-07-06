221b28d74de392617be3cbb2d7625a932d7abb33
Vignesh Natarajan <vickyemailbox@gmail.com>
2021-07-07 01:45:23 +0200

    hidden files import

--- src/scripts/import_utils/import_code.sh
+++ src/scripts/import_utils/import_code.sh
@@ -3,6 +3,6 @@ source ./config.sh
 cd ../
 if [ -n $SOURCE_CODE_LOCATION ]; then
 	echo "importing code and data from ( $SOURCE_CODE_LOCATION ) to ( app_root/app/ )"
-  	cp -arf $SOURCE_CODE_LOCATION/* app_root/app/
+  	cp -arf $SOURCE_CODE_LOCATION/. app_root/app/
 fi
 
