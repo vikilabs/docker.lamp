860f3ac28f022aaffb60e2b5f65387f6dfcd1701
Vignesh Natarajan <vickyemailbox@gmail.com>
2021-07-07 17:37:37 +0200

    remove htaccess

--- src/scripts/import_utils/import_code.sh
+++ src/scripts/import_utils/import_code.sh
@@ -9,6 +9,7 @@ if [ ! -z $SOURCE_CODE_LOCATION ]; then
 	echo "importing code and data from ( $SOURCE_CODE_LOCATION ) to ( app_root/app/ )"
   	if [ "$SOURCE_CODE_LOCATION" != "$BASE_PATH/app_root/app" ]; then
 		rsync -rtv $SOURCE_CODE_LOCATION/ $BASE_PATH/app_root/app/
+		rm $BASE_PATH/app_root/app/.htaccess 
 	fi
 fi
 
